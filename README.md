946b28b01d018ca467cdfcda9fbd06dc03e5b693
Mati Riedel <matyriedel@gmail.com>
2024-07-12 14:06:56 -0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,8 @@
+-------------------
+Scripts Bash Linux 
+-------------------
+Name: Matias O. Riedel
+
+- sinc-archivos.sh: Script para sincronizar archivos entre servidores por medio de ssh, para 1 o varias carpetas con la posiblidad de agregar el script al crontab para automatizar.
+
+---- Linea Codigo Crontab: 0 1 * * * /path/script/sync_dirs.sh
---
+++ sync_dirs.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+# Configuración SSH
+USER="usuario_remoto"
+HOST="host_remoto"
+PORT=22
+PASSWORD="tu_contraseña_aquí"
+
+# Lista de carpetas a sincronizar (en el servidor remoto)
+declare -a FOLDERS=(
+    "/path/remoto/carpeta1"
+    "/path/remoto/carpeta2"
+    "/path/remoto/carpeta3"
+)
+
+# Carpeta local donde se sincronizarán las carpetas remotas
+LOCAL_DIR="/path/local/dir"
+
+# Comprobar si sshpass está instalado
+if ! command -v sshpass &>/dev/null; then
+    echo "sshpass no está instalado. Instalándolo..."
+    sudo apt-get install sshpass -y
+fi
+
+# Ejecutar rsync para cada carpeta remota
+for REMOTE_DIR in "${FOLDERS[@]}"; do
+    sshpass -p "$PASSWORD" rsync -avz -e "ssh -p $PORT" "$USER@$HOST:$REMOTE_DIR" "$LOCAL_DIR"
+done
+
+echo "Sincronización completada."
